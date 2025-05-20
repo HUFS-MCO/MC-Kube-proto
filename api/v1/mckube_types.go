9239781848e05ecb57215254a1a0728ce3db5144
chawoa <>
2025-05-20 19:51:14 +0900

    initial commit

---
+++ api/v1/mckube_types.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// McKubeSpec defines the desired state of McKube
+type McKubeSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of McKube. Edit mckube_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// McKubeStatus defines the observed state of McKube
+type McKubeStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// McKube is the Schema for the mckubes API
+type McKube struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   McKubeSpec   `json:"spec,omitempty"`
+	Status McKubeStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// McKubeList contains a list of McKube
+type McKubeList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []McKube `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&McKube{}, &McKubeList{})
+}
